cae559e0a8150f516dad8142ae68bc6e3777d73e
DESKTOP-IN76A67\Gabriel <gabriel0983@gmail.com>
2021-08-24 20:27:56 +0300

    commit33 checkpoint

--- Game1/Source/Game1/Private/Projectile.cpp
+++ Game1/Source/Game1/Private/Projectile.cpp
@@ -7,16 +7,19 @@ AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
-	ProjectileMovement->bAutoActivate = false;
+
 
 	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh"));
 	SetRootComponent(CollisionMesh);
 	CollisionMesh->SetNotifyRigidBodyCollision(true);
 	CollisionMesh->SetVisibility(true);
 
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
+	ProjectileMovement->bAutoActivate = false;
+
 	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast"));
 	LaunchBlast->AttachTo(RootComponent);
+	LaunchBlast->SetVisibility(true);
 
 }
 
